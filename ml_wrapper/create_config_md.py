3e95bef7444c6f371f5c9ecbac60c53bf318f1db
Jochen Gietzen <jochen.gietzen@inovex.de>
2020-09-02 18:02:06 +0200

    Naming update for create_config_md.py

--- ml_wrapper/create_config_md.py
+++ ml_wrapper/create_config_md.py
@@ -7,11 +7,11 @@ from iniparser import Config
 FILE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 
-def create_config():
+def create_config_markdown():
     """ Creates the config script """
     config = Config(mode="all_allowed").scan(FILE_DIR, True).read()
     config.to_env("./env_ml_wrapper.md")
 
 
 if __name__ == "__main__":
-    create_config()
+    create_config_markdown()
