55a9482309c5707ad939231cb2cbf2a354da1e12
Jochen Gietzen <jochen.gietzen@inovex.de>
2020-09-10 14:31:29 +0200

    Proper setup.py

--- src/ml_wrapper/__init__.py
+++ src/ml_wrapper/__init__.py
@@ -5,6 +5,9 @@ Init file that organizes the base level imports
 
 from pkg_resources import get_distribution, DistributionNotFound
 from .json_provider import *
+from .result_type import *
+from .message_type import *
+from .messaging import *
 from .ml_wrapper import MLWrapper
 
 try:
