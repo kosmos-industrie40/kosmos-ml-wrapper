afb12925b357cf0aa2459ca8ff13b0bbed645736
Jochen Gietzen <jochen.gietzen@inovex.de>
2020-10-16 08:37:29 +0200

    Template update to remove src.

--- templates/{{ cookiecutter.project_name_in_src }}/__main__.py
+++ templates/{{ cookiecutter.project_name_in_src }}/__main__.py
@@ -1,7 +1,7 @@
 """
 Execution of the {{ cookiecutter.ml_tool_name }}
 """
-from src.{{ cookiecutter.ml_tool_name }} import {{ cookiecutter.ml_class_name }}
+from {{ cookiecutter.project_name_in_src }}.{{ cookiecutter.ml_tool_name }} import {{ cookiecutter.ml_class_name }}
 
 # Usage of Heuristic class
 if __name__ == "__main__":
