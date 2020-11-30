ad0f49d5af3e0ceea7456a00039054ba9a992dfe
Jochen Gietzen <jochen.gietzen@inovex.de>
2020-11-30 13:24:08 +0100

    Added default value of env test case
    Fixes #29

--- tests/configurations/test_env_configs.py
+++ tests/configurations/test_env_configs.py
@@ -1,7 +1,6 @@
 """
 This module tests configurations of the ML Wrapper
 """
-
 from tests.conftest import FftMock
 
 
@@ -10,3 +9,9 @@ def test_empty_config_request_topic(monkeypatch, ML_MOCK_FFT):
     mock_ = FftMock(outgoing_message_is_temporary=True)
     assert len(mock_._get_topics()) == 1
     assert mock_._get_topics()[0] == "kosmos/analytics/test_url/test_tag"
+
+
+def test_empty_config_request_topic_unset(monkeypatch, ML_MOCK_FFT):
+    mock_ = FftMock(outgoing_message_is_temporary=True)
+    assert len(mock_._get_topics()) == 3
+    assert mock_._get_topics()[-1] == "kosmos/analytics/test_url/test_tag"
